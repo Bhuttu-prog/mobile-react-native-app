7af2b205101ad456a0fce89cd786050d8a870e5a
Bhuttu-prog <diksha.chaudhri8@gmail.com>
2024-10-03 17:40:12 +0530

    committing to main app file

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Hello World</Text>
+      <Text>Hello World - main branch</Text>
       <StatusBar style="auto" />
     </View>
   );
