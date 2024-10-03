a65b8801f234a3e530be9d2ecf72e62a7e0a8aee
Bhuttu-prog <diksha.chaudhri8@gmail.com>
2024-10-03 17:42:47 +0530

    [feature] | commit to app file

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Hello World - main branch</Text>
+      <Text>Hello World - main branch - sub branch = feature</Text>
       <StatusBar style="auto" />
     </View>
   );
