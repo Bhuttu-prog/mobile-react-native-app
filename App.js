34eca56ddff797ea0e9c03caff55262e83dd6911
Bhuttu-prog <diksha.chaudhri8@gmail.com>
2024-10-03 17:36:26 +0530

    react native app initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Hello World</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
